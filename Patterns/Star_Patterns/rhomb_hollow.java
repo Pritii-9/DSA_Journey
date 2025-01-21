efca116c5d8817da08636b38fa6b886c41a8aaca
Priti Jadhav <pvjadhav2513@gmail.com>
2025-01-21 06:22:09 +0530

    added to git

---
+++ Patterns/Star_Patterns/rhomb_hollow.java
@@ -0,0 +1,24 @@
+public class rhomb_hollow {
+
+    public static void printHollowRhombus(int n) {
+        for (int i = 1; i <= n; i++) {
+            // Print leading spaces
+            for (int j = 1; j <= n - i; j++) {
+                System.out.print(" ");
+            }
+            
+            // Print hollow rhombus pattern
+            for (int j = 1; j <= n; j++) {
+                if (i == 1 || i == n || j == 1 || j == n) {
+                    System.out.print("* ");
+                } else {
+                    System.out.print("  ");
+                }
+            }
+            System.out.println();
+        }
+    }
+    public static void main(String[] args) {
+        printHollowRhombus(5);
+    }
+}
